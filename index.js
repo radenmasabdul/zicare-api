8db1ff4a03eaa5bf8d13e03ec2f163385e9bb61f
radenmasabdul <abdul.tamsis@gmail.com>
2025-07-23 22:15:52 +0700

    Inisialisasi

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
