3c5eee406ca83bc57c859a5d9a8dd904a670ed00
radenmasabdul <abdul.tamsis@gmail.com>
2025-07-27 02:05:10 +0700

    fix: invalid token

--- src/middlewares/auth/auth.js
+++ src/middlewares/auth/auth.js
@@ -2,8 +2,13 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ message: 'Unauthenticated.' });
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: 'Unauthenticated.' });
+    }
+
+    const token = authHeader.split(' ')[1];
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) return res.status(401).json({ message: 'Invalid token.' });
